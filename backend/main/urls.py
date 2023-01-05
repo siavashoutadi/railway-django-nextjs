84b52789436d121007e5452ffb02d278775dbb64
Siavash Outadi <siavash.outadi@gmail.com>
2023-01-05 18:11:28 +0100

    Disable graphiql

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -6,7 +6,7 @@ from strawberry.django.views import GraphQLView
 from .schema import schema
 
 urlpatterns = [
-    path('graphql', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
+    path('graphql', csrf_exempt(GraphQLView.as_view(graphiql=False, schema=schema))),
     path('admin/', admin.site.urls),
     path('ht/M4ZUFrstgmRNMfJfclTsyCe6Xosmd36EE2NxeU4CDUg',
          include('health_check.urls'))
