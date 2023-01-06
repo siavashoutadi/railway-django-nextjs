62c0b34684f275388d5ce6ad8f4f6f81c277882e
Siavash Outadi <siavash.outadi@gmail.com>
2023-01-06 16:23:06 +0100

    Add storybook

---
+++ frontend/.storybook/preview.js
@@ -0,0 +1,17 @@
+// Allow Storybook to handle Next's <Image> component
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
