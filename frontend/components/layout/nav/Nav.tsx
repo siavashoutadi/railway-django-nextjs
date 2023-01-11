108d47b4ea3bb451e2ca4d3479d5038732ab3f8d
Siavash Outadi <siavash.outadi@gmail.com>
2023-01-11 08:59:24 +0100

    Add prettier for tailwind

--- frontend/components/layout/nav/Nav.tsx
+++ frontend/components/layout/nav/Nav.tsx
@@ -3,9 +3,9 @@ export interface INav {}
 
 const Nav: React.FC<INav> = ({}) => {
   return (
-    <nav className="bg-gray-200 fixed bottom-0 lg:bottom-auto lg:top-0 w-full text-gray-800 lg:opacity-75 z-10">
+    <nav className="fixed bottom-0 z-10 w-full bg-gray-200 text-gray-800 lg:bottom-auto lg:top-0 lg:opacity-75">
       <div className="container">
-        <div className="flex justify-around lg:justify-end py-2">
+        <div className="flex justify-around py-2 lg:justify-end">
           <NavItem href="/login" text="Login" icon="LoginIcon" />
           <NavItem href="/" text="Home" icon="HomeIcon" />
         </div>
