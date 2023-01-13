e72cff70a05737f469047b3edf0fc843e7f5d204
Siavash Outadi <siavash.outadi@gmail.com>
2023-01-13 17:56:09 +0100

    Change the style for 404 page

--- frontend/pages/404.tsx
+++ frontend/pages/404.tsx
@@ -1,11 +1,11 @@
 export default function Custom404() {
   return (
-    <div className="flex h-screen w-full flex-col justify-center text-center align-middle">
-      <p className="text-base font-semibold">404</p>
-      <h1 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+    <div className="absolute left-0 top-0 -z-10 flex h-screen w-full flex-col justify-center text-center align-middle">
+      <p className="text-8xl font-bold text-brand-500">404</p>
+      <h1 className="mt-2 text-4xl font-semibold tracking-tight sm:text-5xl">
         This page does not exist.
       </h1>
-      <p className="mt-2 text-lg text-gray-500">
+      <p className="mt-2 text-lg">
         The page you are looking for could not be found.
       </p>
     </div>
